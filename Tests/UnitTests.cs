04c3c475d902043fde85a99ad4fe6934891138d0
RaoulTayo <98632085+RaoulTayo@users.noreply.github.com>
2022-03-31 19:08:37 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -31,5 +31,31 @@ namespace GithubActionsLab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+        
+        
+        [Test]
+        public void Subtract_Valid()
+        {
+            Assert.AreEqual(1, Program.Add("2", "1"));
+            Assert.AreEqual(2, Program.Add("5", "3"));
+            Assert.AreEqual(3, Program.Add("10", "7"));
+        }
+
+        [Test]
+        public void Subtract_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+       
     }
 }
